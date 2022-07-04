68a0969d907e659aa7d7113f7c8779192f3e2046
1337-server <sndspamfilter@gmail.com>
2022-07-04 16:26:30 -0700

    Fix build (#19)
    * Add docker arguments for labels
    
    * add health check 
    
    * don't push on PR

--- scripts/install_handbrake.sh
+++ scripts/install_handbrake.sh
@@ -20,23 +20,25 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 apkArch="$(dpkg --print-architecture)"
-
-# If we're not arm64 install standard HandBrakeCLI and exit cleanly
-if [ ! "$apkArch" = "amd64" ]; then
-    apt install handbrake-cli
-    exit 0
-fi
+# curl --silent 'https://github.com/automatic-ripping-machine/automatic-ripping-machine/releases' | grep automatic-ripping-machine/tree/* | head -n 1 | sed -e 's/[^0-9\.]*//g'
 # https://github.com/HandBrake/HandBrake/releases
-HANDBRAKE_VERSION=$(curl --silent 'https://github.com/HandBrake/HandBrake/releases' | grep HandBrake/tree/1.5.1 | head -n 1 | sed -e 's/[^0-9\.]*//g')
+HANDBRAKE_VERSION=$(curl --silent 'https://github.com/HandBrake/HandBrake/releases' | grep 'HandBrake/tree/*' | head -n 1 | sed -e 's/[^0-9\.]*//g')
 sleep 1
 set -ex
-savedAptMark="$(apt-mark showmanual)"
 export PREFIX='/usr/local'
+apt-get update && apt-get upgrade -yqq
+# If we're not arm64 install standard HandBrakeCLI and exit cleanly
+if [ ! "$apkArch" = "amd64" ]; then
+  echo "Running on arm - using apt for HandBrakeCLI"
+  apt install -yqq handbrake-cli
+  cp /usr/bin/HandBrakeCLI /usr/local/bin/HandBrakeCLI
+  exit 0
+fi
 #################################################################################################
 set -eux
-savedAptMark="$(apt-mark showmanual)"
-apt-get update
-apt-get install -y autoconf automake autopoint appstream build-essential cmake git libass-dev libbz2-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libharfbuzz-dev libjansson-dev liblzma-dev libmp3lame-dev libnuma-dev libogg-dev libopus-dev libsamplerate-dev libspeex-dev libtheora-dev libtool libtool-bin libturbojpeg0-dev libvorbis-dev libx264-dev libxml2-dev libvpx-dev m4 make meson nasm ninja-build patch pkg-config python tar zlib1g-dev libva-dev libdrm-dev yasm libvpx. libx264.
+apt-get install -yqq --no-install-recommends autoconf automake autopoint appstream build-essential cmake git wget libass-dev libbz2-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libharfbuzz-dev libjansson-dev liblzma-dev libmp3lame-dev libnuma-dev libogg-dev libopus-dev libsamplerate-dev libspeex-dev libtheora-dev libtool libtool-bin libturbojpeg0-dev libvorbis-dev libx264-dev libxml2-dev libvpx-dev m4 make meson nasm ninja-build patch pkg-config python tar zlib1g-dev libva-dev libdrm-dev yasm libvpx. libx264. \
+&& apt-get -yqq autoremove \
+&& apt-get clean
 rm -rf /var/lib/apt/lists/*
 wget -O handbrake.tar.bz2.sig "https://github.com/HandBrake/HandBrake/releases/download/$HANDBRAKE_VERSION/HandBrake-$HANDBRAKE_VERSION-source.tar.bz2.sig"
 wget -O handbrake.tar.bz2 "https://github.com/HandBrake/HandBrake/releases/download/$HANDBRAKE_VERSION/HandBrake-$HANDBRAKE_VERSION-source.tar.bz2"
@@ -53,10 +55,10 @@ tar --extract \
 	"HandBrake-$HANDBRAKE_VERSION"
 rm handbrake.tar.bz2
 cd /tmp/handbrake
-#./configure --disable-gtk --enable-qsv --enable-vce --launch-jobs=$(nproc) --launch
 nproc="$(nproc)"
 ./configure --disable-gtk --enable-qsv --enable-vce --launch-jobs="$nproc" --launch
 make -C build -j "$nproc"
 make -C build install
 cd /
 rm -rf /tmp/handbrake
+apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false
