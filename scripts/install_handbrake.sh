23370eaed288046fc35a2a359632d20587ed4123
1337-server <sndspamfilter@gmail.com>
2023-09-10 13:52:01 +0100

    Moved the config of both ripper and apprise into database
    Added 15s sleep to docker_arm_wrapper.sh
    
    Moved api to its own folder to make things easier to manage
    
    Fixed job status on cards
    
    Tried to slim down the image size slightly without impacting functions
    
    Log view folder now works as expected
    
    Fixed error with MANUAL_WAIT being declared as an int instead of a bool

--- scripts/install_handbrake.sh
+++ scripts/install_handbrake.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+# if architecture is any flavor of arm, install standard HandBrakeCLI and exit cleanly
+if [[ $(dpkg --print-architecture) =~ arm.* ]]; then
+    echo "Running on arm - using apt for HandBrakeCLI"
+    exit 0
+fi
 # Setup taken from https://github.com/tianon/dockerfiles/blob/master/handbrake/Dockerfile
 # The Expat/MIT License
 #
@@ -20,40 +24,96 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
-
 echo -e "${RED}Finding current HandBrake version${NC}"
 HANDBRAKE_VERSION=$(curl --silent 'https://github.com/HandBrake/HandBrake/releases' | grep 'HandBrake/tree/*' | head -n 1 | sed -e 's/[^0-9\.]*//g')
 echo -e "${RED}Downloading HandBrake $HANDBRAKE_VERSION${NC}"
-
-# if architecture is any flavor of arm, install standard HandBrakeCLI and exit cleanly
-if [[ $(dpkg --print-architecture) =~ arm.* ]]; then
-    echo "Running on arm - using apt for HandBrakeCLI"
-    exit 0
-fi
-
-set -eux
-wget -O handbrake.tar.bz2.sig "https://github.com/HandBrake/HandBrake/releases/download/$HANDBRAKE_VERSION/HandBrake-$HANDBRAKE_VERSION-source.tar.bz2.sig"
-wget -O handbrake.tar.bz2 "https://github.com/HandBrake/HandBrake/releases/download/$HANDBRAKE_VERSION/HandBrake-$HANDBRAKE_VERSION-source.tar.bz2"
-
+set -eux; \
+	savedAptMark="$(apt-mark showmanual)"; \
+	apt-get update; \
+	apt-get install -y --no-install-recommends \
+		bzip2 \
+		ca-certificates \
+		gnupg \
+		wget \
+		\
+		autoconf \
+		automake \
+		autopoint \
+		binutils \
+		cmake \
+		g++ \
+		gcc \
+		gettext \
+		libass-dev \
+		libbz2-dev \
+		libc6-dev \
+		libdrm-dev \
+		libgtk-3-dev \
+		libjansson-dev \
+		liblzma-dev \
+		libmp3lame-dev \
+		libnuma-dev \
+		libopus-dev \
+		libspeex-dev \
+		libtheora-dev \
+		libtool-bin \
+		libturbojpeg-dev \
+		libva-dev \
+		libvorbis-dev \
+		libvpx-dev \
+		libx264-dev \
+		libxml2-dev \
+		make \
+		meson \
+		nasm \
+		patch \
+		pkg-config \
+		python3 \
+	; \
+	rm -rf /var/lib/apt/lists/*; \
+	\
+	wget -O handbrake.tar.bz2.sig "https://github.com/HandBrake/HandBrake/releases/download/$HANDBRAKE_VERSION/HandBrake-$HANDBRAKE_VERSION-source.tar.bz2.sig"; \
+	wget -O handbrake.tar.bz2 "https://github.com/HandBrake/HandBrake/releases/download/$HANDBRAKE_VERSION/HandBrake-$HANDBRAKE_VERSION-source.tar.bz2"; \
+	\
 # https://handbrake.fr/openpgp.php or https://github.com/HandBrake/HandBrake/wiki/OpenPGP
-GNUPGHOME="$(mktemp -d)" && export GNUPGHOME
-gpg --batch --keyserver keyserver.ubuntu.com --recv-keys '1629 C061 B3DD E7EB 4AE3  4B81 021D B8B4 4E4A 8645'; \
-gpg --batch --verify handbrake.tar.bz2.sig handbrake.tar.bz2; \
-rm -rf "$GNUPGHOME" handbrake.tar.bz2.sig
-mkdir -p /tmp/handbrake
-tar --extract \
-	--file handbrake.tar.bz2 \
-	--directory /tmp/handbrake \
-	--strip-components 1 \
-	"HandBrake-$HANDBRAKE_VERSION"
-rm handbrake.tar.bz2
-
-# build
-cd /tmp/handbrake
-nproc="$(nproc)"
-./configure --disable-gtk --enable-qsv --enable-vce --launch-jobs="$nproc" --launch
-make -C build -j "$nproc"
-make -C build install
-cp /usr/local/bin/HandBrakeCLI /usr/bin/HandBrakeCLI
-cd /
-rm -rf /tmp/handbrake
+	GNUPGHOME="$(mktemp -d)"; export GNUPGHOME; \
+	gpg --batch --keyserver keyserver.ubuntu.com --recv-keys '1629 C061 B3DD E7EB 4AE3  4B81 021D B8B4 4E4A 8645'; \
+	gpg --batch --verify handbrake.tar.bz2.sig handbrake.tar.bz2; \
+	gpgconf --kill all; \
+	rm -rf "$GNUPGHOME" handbrake.tar.bz2.sig; \
+	\
+	mkdir -p /tmp/handbrake; \
+	tar --extract \
+		--file handbrake.tar.bz2 \
+		--directory /tmp/handbrake \
+		--strip-components 1 \
+		"HandBrake-$HANDBRAKE_VERSION" \
+	; \
+	rm handbrake.tar.bz2; \
+	\
+	cd /tmp/handbrake; \
+	./configure --disable-gtk --enable-qsv --enable-vce --launch-jobs=$(nproc) --launch;
+	\
+	nproc="$(nproc)"; \
+	make -C build -j "$nproc"; \
+	make -C build install; \
+	\
+	cd /; \
+	rm -rf /tmp/handbrake; \
+	\
+	apt-mark auto '.*' > /dev/null; \
+	[ -z "$savedAptMark" ] || apt-mark manual $savedAptMark > /dev/null; \
+	find /usr/local \
+		-type f \
+		\( -executable -o -name '*.so' \) \
+		-exec ldd '{}' ';' \
+		| awk '/=>/ { print $(NF-1) }' \
+		| sort -u \
+		| xargs -r dpkg-query --search \
+		| cut -d: -f1 \
+		| sort -u \
+		| xargs -r apt-mark manual \
+	; \
+	apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false; \
+	\
+	HandBrakeCLI --version;
--- scripts/install_makemkv.sh
+++ scripts/install_makemkv.sh
@@ -26,47 +26,129 @@ echo -e "${RED}Finding current MakeMKV version${NC}"
 MAKEMKV_VERSION=$(curl -s https://www.makemkv.com/download/ | grep -o "[0-9.]*.txt" | sed 's/.txt//')
 echo -e "${RED}Downloading MakeMKV $MAKEMKV_VERSION sha, bin, and oss${NC}"
 
-set -ex
-savedAptMark="$(apt-mark showmanual)"
+# This disc requires Java runtime (JRE), but none was found. Certain functions will fail, please install Java. See http://www.makemkv.com/bdjava/ for details.
+set -eux; \
+	apt-get update; \
+	apt-get install -y --no-install-recommends \
+		openjdk-11-jre-headless \
+	; \
+	rm -rf /var/lib/apt/lists/*
 
-wget -O 'sha256sums.txt.sig' "https://www.makemkv.com/download/makemkv-sha-${MAKEMKV_VERSION}.txt"
-GNUPGHOME="$(mktemp -d)" && export GNUPGHOME
-gpg --batch --keyserver keyserver.ubuntu.com --recv-keys 2ECF23305F1FC0B32001673394E3083A18042697
-gpg --batch --decrypt --output sha256sums.txt sha256sums.txt.sig
-gpgconf --kill all
-rm -rf "$GNUPGHOME" sha256sums.txt.sig
 
-export PREFIX='/usr/local'
-for ball in makemkv-oss makemkv-bin; do
-	wget -O "$ball.tgz" "https://www.makemkv.com/download/${ball}-${MAKEMKV_VERSION}.tar.gz"
-	sha256="$(grep "  $ball-${MAKEMKV_VERSION}[.]tar[.]gz\$" sha256sums.txt)"
-	sha256="${sha256%% *}"
-	[ -n "$sha256" ]
-	echo "$sha256 *$ball.tgz" | sha256sum -c -
-	mkdir -p "$ball"
-	tar -xvf "$ball.tgz" -C "$ball" --strip-components=1
-	rm "$ball.tgz"
-	cd "$ball"
-	if [ -f configure ]; then
-		./configure --prefix="$PREFIX"
-	else
-		mkdir -p tmp
-		touch tmp/eula_accepted
-	fi
-	make -j "$(nproc)" PREFIX="$PREFIX"
-	make install PREFIX="$PREFIX"
-	cd ..
-	rm -r "$ball"
-done
+# beta code: http://www.makemkv.com/forum2/viewtopic.php?f=5&t=1053
+set -ex; \
+	\
+	savedAptMark="$(apt-mark showmanual)"; \
+	\
+	apt-get update; \
+	apt-get install -y --no-install-recommends \
+		ca-certificates \
+		g++ \
+		gcc \
+		gnupg \
+		libavcodec-dev \
+		libexpat-dev \
+		libssl-dev \
+		make \
+		pkg-config \
+		qtbase5-dev \
+		wget \
+		zlib1g-dev \
+	; \
+	rm -rf /var/lib/apt/lists/*; \
+	\
+	wget -O 'sha256sums.txt.sig' "https://www.makemkv.com/download/makemkv-sha-${MAKEMKV_VERSION}.txt"; \
+# TODO pre-download (in versions.sh)/verify sha256sums
+# pub   dsa2048 2017-05-26 [SC]
+#       2ECF 2330 5F1F C0B3 2001  6733 94E3 083A 1804 2697
+# uid           [ unknown] MakeMKV (signature) <support@makemkv.com>
+	export GNUPGHOME="$(mktemp -d)"; \
+	gpg --batch --keyserver keyserver.ubuntu.com --recv-keys 2ECF23305F1FC0B32001673394E3083A18042697; \
+	gpg --batch --decrypt --output sha256sums.txt sha256sums.txt.sig; \
+	gpgconf --kill all; \
+	rm -rf "$GNUPGHOME" sha256sums.txt.sig; \
+	\
+	export PREFIX='/usr/local'; \
+	for ball in makemkv-oss makemkv-bin; do \
+		wget -O "$ball.tgz" "https://www.makemkv.com/download/${ball}-${MAKEMKV_VERSION}.tar.gz"; \
+		sha256="$(grep "  $ball-${MAKEMKV_VERSION}[.]tar[.]gz\$" sha256sums.txt)"; \
+		sha256="${sha256%% *}"; \
+		[ -n "$sha256" ]; \
+		echo "$sha256 *$ball.tgz" | sha256sum -c -; \
+		\
+		mkdir -p "$ball"; \
+		tar -xvf "$ball.tgz" -C "$ball" --strip-components=1; \
+		rm "$ball.tgz"; \
+		\
+		cd "$ball"; \
+		if [ -f configure ]; then \
+			./configure --prefix="$PREFIX"; \
+		else \
+			mkdir -p tmp; \
+			touch tmp/eula_accepted; \
+		fi; \
+		make -j "$(nproc)" PREFIX="$PREFIX"; \
+		make install PREFIX="$PREFIX"; \
+		\
+		cd ..; \
+		rm -r "$ball"; \
+	done; \
+	\
+	rm sha256sums.txt; \
+	\
+	apt-mark auto '.*' > /dev/null; \
+	[ -z "$savedAptMark" ] || apt-mark manual $savedAptMark; \
+	find /usr/local -type f -executable -exec ldd '{}' ';' \
+		| awk '/=>/ { print $(NF-1) }' \
+		| sort -u \
+		| xargs -r dpkg-query --search \
+		| cut -d: -f1 \
+		| sort -u \
+		| xargs -r apt-mark manual \
+	; \
+	apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false
 
-rm sha256sums.txt
-apt-mark auto '.*' > /dev/null
-# shellcheck disable=SC2086
-[ -z "$savedAptMark" ] || apt-mark manual $savedAptMark # double quoting this var breaks the build
-find /usr/local -type f -executable -exec ldd '{}' ';' \
-	| awk '/=>/ { print $(NF-1) }' \
-	| sort -u \
-	| xargs -r dpkg-query --search \
-	| cut -d: -f1 \
-	| sort -u \
-	| xargs -r apt-mark manual
+#set -ex
+#savedAptMark="$(apt-mark showmanual)"
+#
+#wget -O 'sha256sums.txt.sig' "https://www.makemkv.com/download/makemkv-sha-${MAKEMKV_VERSION}.txt"
+#GNUPGHOME="$(mktemp -d)" && export GNUPGHOME
+#gpg --batch --keyserver keyserver.ubuntu.com --recv-keys 2ECF23305F1FC0B32001673394E3083A18042697
+#gpg --batch --decrypt --output sha256sums.txt sha256sums.txt.sig
+#gpgconf --kill all
+#rm -rf "$GNUPGHOME" sha256sums.txt.sig
+#
+#export PREFIX='/usr/local'
+#for ball in makemkv-oss makemkv-bin; do
+#	wget -O "$ball.tgz" "https://www.makemkv.com/download/${ball}-${MAKEMKV_VERSION}.tar.gz"
+#	sha256="$(grep "  $ball-${MAKEMKV_VERSION}[.]tar[.]gz\$" sha256sums.txt)"
+#	sha256="${sha256%% *}"
+#	[ -n "$sha256" ]
+#	echo "$sha256 *$ball.tgz" | sha256sum -c -
+#	mkdir -p "$ball"
+#	tar -xvf "$ball.tgz" -C "$ball" --strip-components=1
+#	rm "$ball.tgz"
+#	cd "$ball"
+#	if [ -f configure ]; then
+#		./configure --prefix="$PREFIX"
+#	else
+#		mkdir -p tmp
+#		touch tmp/eula_accepted
+#	fi
+#	make -j "$(nproc)" PREFIX="$PREFIX"
+#	make install PREFIX="$PREFIX"
+#	cd ..
+#	rm -r "$ball"
+#done
+#
+#rm sha256sums.txt
+#apt-mark auto '.*' > /dev/null
+## shellcheck disable=SC2086
+#[ -z "$savedAptMark" ] || apt-mark manual $savedAptMark # double quoting this var breaks the build
+#find /usr/local -type f -executable -exec ldd '{}' ';' \
+#	| awk '/=>/ { print $(NF-1) }' \
+#	| sort -u \
+#	| xargs -r dpkg-query --search \
+#	| cut -d: -f1 \
+#	| sort -u \
+#	| xargs -r apt-mark manual
